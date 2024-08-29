dcdb25947dd62304d98dde84ddc9ab547e956c30
Rishi Koul <rishikoul@Rishis-MacBook-Air.local>
2024-08-28 22:56:54 -0400

    done with the bot

--- main.py
+++ main.py
@@ -8,56 +8,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
 import os
-from dotenv import load_dotenv, dotenv_values 
+from dotenv import load_dotenv, dotenv_values
 
 load_dotenv() 
 
-chrome_options = Options()
-chrome_options.add_argument("--incognito")
-
-service = Service("./chromedriver")
-driver = webdriver.Chrome(service=service)
-
-date_today = datetime.today().strftime("%-m/%d/%Y")
-
-date_two = datetime.today() + timedelta(days=2)
-date_two = date_two.strftime("%-m/%d/%Y")
-
-driver.get("https://www.buildinglink.com/v2/tenant/amenities/NewReservation.aspx?from=1&amenityId=" + 
-           os.getenv("TENNIS_AMENTIY_ID") + "&starts=" + date_two + "%204:00:00%20PM&ends=" + date_today + "%205:00:00%20PM")
-
-# Make sure we are at the login page
-WebDriverWait(driver, 5).until(
-    EC.presence_of_all_elements_located((By.ID, "UserName"))
-)
-# Enter login details
-login_element = driver.find_element(By.ID, "UserName")
-login_element.send_keys(os.getenv("USERNAME"))
-
-password_element = driver.find_element(By.ID, "Password")
-password_element.send_keys(os.getenv("PASSWORD"))
-
-login_button = driver.find_element(By.ID, "LoginButton")
-login_button.click()
-
-# open the date picker
-date_picker = driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_StartTimePicker_dateInput")
-date_picker.send_keys("value", "5:00")
-
-# select 5pm as time
-five_pm = driver.find_element(By.XPATH, "//*[contains(text(), '5:00 PM')]")
-five_pm.click()
-
-if(not driver.find_elements(By.XPATH, "//*[contains(text(), 'I agree to the above')]")):
+while(datetime.now().hour != 0):
     pass
-else:
-    terms = driver.find_element(By.XPATH, "//*[contains(text(), 'I agree to the above')]/ancestor::span/span/input")
-    terms.click()
+if(datetime.now().hour == 0):
+    date_today = datetime.today().strftime("%-m/%d/%Y")
+    date_two = datetime.today() + timedelta(days=2)
+    date_two_string = date_two.strftime("%-m/%d/%Y")
+    date_two = date_two.date()
+
+    chrome_options = Options()
+    chrome_options.add_argument("--incognito")
+
+    service = Service("./chromedriver")
+    driver = webdriver.Chrome(service=service)
+    driver.get("https://www.buildinglink.com/v2/tenant/amenities/NewReservation.aspx?from=1&amenityId=" + 
+            os.getenv("TENNIS_AMENTIY_ID") + "&starts=" + date_two_string + "%204:00:00%20PM&ends=" + date_two_string + "%205:00:00%20PM")
+
+    # Make sure we are at the login page
+    WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.ID, "UserName"))
+    )
+    # Enter login details
+    login_element = driver.find_element(By.ID, "UserName")
+    login_element.send_keys(os.getenv("USERNAME"))
+
+    password_element = driver.find_element(By.ID, "Password")
+    password_element.send_keys(os.getenv("PASSWORD"))
+
+    login_button = driver.find_element(By.ID, "LoginButton")
+    login_button.click()
+
+    # open the date picker
+    date_picker = driver.find_element(By.ID, "ctl00_ContentPlaceHolder1_StartTimePicker_dateInput")
+    date_picker.send_keys("value", "5:00")
+
+    # select 5pm as time
+    five_pm = driver.find_element(By.XPATH, "//*[contains(text(), '5:00 PM')]")
+    five_pm.click()
+
+    if(not driver.find_elements(By.XPATH, "//*[contains(text(), 'I agree to the above')]")):
+        pass
+    else:
+        terms = driver.find_element(By.XPATH, "//*[contains(text(), 'I agree to the above')]/ancestor::span/span/input")
+        terms.click()
 
 
-# Need to double check the time is correct, date is always correct
-save_button = driver.find_element(By.XPATH, "//*[contains(text(), 'Save')]/ancestor::a")
-save_button.click()
+    # Need to double check the time is correct, date is always correct
+    save_button = driver.find_element(By.XPATH, "//*[contains(text(), 'Save')]/ancestor::a")
+    save_button.click()
 
-driver.quit()
+    driver.quit()
 
